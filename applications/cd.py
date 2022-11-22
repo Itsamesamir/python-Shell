66195ab474fcfe051acf8ccdf4236106786294d4
Tilen Limback-Stokin <tls@TilensMacBookPro.cust.communityfibre.co.uk>
2022-11-22 18:19:13 +0000

    Made grammar work for applications and input stream integrated into visitor.

--- applications/cd.py
+++ applications/cd.py
@@ -1,10 +1,6 @@
 from abstractFactory import abstract_application, os
 
-class cd(abstract_application):
-    def exec(args, input, output):
-        if len(args) == 0 or len(args) > 1:
-                raise ValueError("wrong number of command line arguments")
-        return os.chdir(args[0])
 
-        # input would be [dir] with options
-        # -P, -L or -e
+class cd(abstract_application):
+    def execute(args):
+        pass
--- src/main.py
+++ src/main.py
@@ -7,46 +7,94 @@ from dist.ShellGrammarVisitor import ShellGrammarVisitor
 from antlr4.tree.Trees import Trees
 
 
+def getIndex(stream):
+    tmp = []
+    for token in stream.tokens[:-1]:
+        tmp.append(token.tokenIndex)
+
+    return tmp
+
+
+def getType(stream):
+    tmp = []
+    for token in stream.tokens[:-1]:
+        tmp.append(token.type)
+
+    return tmp
+
+
+def getText(stream):
+    tmp = []
+    for token in stream.tokens[:-1]:
+        tmp.append(token.text)
+
+    return tmp
+
+
 class MyVisitor(ShellGrammarVisitor):
     def visitPwd(self, ctx):
         return os.getcwd()
-    
+
     def visitExit(self, ctx):
         sys.exit(0)
 
-    def visitCd(self, ctx: ShellGrammarParser.CdContext):
-        return super().visitCd(ctx)
+    def visitCd(self, ctx):
+        textList = getText(stream)
+        if len(textList) > 2:
+            raise ValueError("Wrong number of arguments")
+        try:
+            os.chdir(textList[1])
+        except FileNotFoundError:
+            print("File not found")
 
-    def visitEcho(self, ctx: ShellGrammarParser.EchoContext):
+    def visitEcho(self, ctx):
         return super().visitEcho(ctx)
 
-    def visitLs(self, ctx: ShellGrammarParser.LsContext):
-        return super().visitLs(ctx)
+    def visitLs(self, ctx):
+        textList = getText(stream)
+        if len(textList) == 2:
+            ls_dir = os.chdir(textList[1])
+        elif len(textList) > 2:
+            raise ValueError("wrong number of command line arguments")
+        else:
+            ls_dir = os.getcwd()
+        for f in os.listdir(ls_dir):
+            print(f, end=" ")
 
-    def visitCat(self, ctx: ShellGrammarParser.CatContext):
+    def visitCat(self, ctx):
         return super().visitCat(ctx)
 
-    def visitHead(self, ctx: ShellGrammarParser.HeadContext):
+    def visitHead(self, ctx):
         return super().visitHead(ctx)
 
-    def visitTail(self, ctx: ShellGrammarParser.TailContext):
+    def visitTail(self, ctx):
         return super().visitTail(ctx)
 
-    def visitGrep(self, ctx: ShellGrammarParser.GrepContext):
+    def visitGrep(self, ctx):
+        print(getText(stream))
+        print(getIndex(stream))
         return super().visitGrep(ctx)
 
 
 if __name__ == "__main__":
     while 1:
-        data = InputStream(input(">>> "))
+        data = InputStream(input("8==D "))
         # lexer
         lexer = ShellGrammarLexer(data)
         stream = CommonTokenStream(lexer)
+        stream.fill()
         # parser
         parser = ShellGrammarParser(stream)
         tree = parser.start()
         # evaluator
         visitor = MyVisitor()
         output = visitor.visit(tree)
-        print(output)
-        print(Trees.toStringTree(tree, None, parser))
+
+        print(f"\n {Trees.toStringTree(tree, None, parser)}")
+
+        # stream.fill()
+        # print("{:<30} {:<10} {:<10} {:<10}".format(
+        #     'Text', 'Type', 'Index', 'Column'))
+        # for token in stream.tokens[:-1]:
+        #     print("{:<30} {:<10} {:<10} {:<10}".format(
+        #         token.text, token.type, token.tokenIndex, token.column))
